49e79038b771606aa90a62bf497d7dbb54c6a2d9
Thang Hoang Ngoc <thanghoangngoc@mmms-MacBook-Pro.local>
2018-11-21 11:43:21 +0700

    init redux in project

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import Main from './src/Main';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <Provider store={store}>
         <Main />
-      </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-});
+const reducer = (state = 0, action) => {
+  return state;
+}
+
+const store = createStore(reducer);
+
+
